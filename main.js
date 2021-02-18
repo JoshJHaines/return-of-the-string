91c2bfbd91e2b6f3de749180a479bf5dad4e5378
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-18 14:37:42 -0500

    just ran prittier for cleaner look

--- main.js
+++ main.js
@@ -1,84 +1,83 @@
 /*******************
  * YOUR CODE HERE! *
  *******************/
-function yell(string){
-  string = string + "!";
-  return string
+function yell(string) {
+	string = string + "!";
+	return string;
 }
 
-function getFirstCharacter(string){
-  return string[0]
+function getFirstCharacter(string) {
+	return string[0];
 }
 
-function getLastCharacter(string){
-  return string[string.length-1]
+function getLastCharacter(string) {
+	return string[string.length - 1];
 }
 
-function getOneCharacter(string, num){
-  return string[num]
+function getOneCharacter(string, num) {
+	return string[num];
 }
 
-function getTwoCharacters(string, num1, num2){
-  return string[num1] + string[num2]
+function getTwoCharacters(string, num1, num2) {
+	return string[num1] + string[num2];
 }
 
-function makeCapitalized(string){
-  return string.toUpperCase()
+function makeCapitalized(string) {
+	return string.toUpperCase();
 }
 
-function yellLouder(string){
-  return string.toUpperCase() + "!!!"
+function yellLouder(string) {
+	return string.toUpperCase() + "!!!";
 }
 
-function getInitials(string){
-  let x = string.indexOf(' ') + 1;
-  return string[0] + "." + string.charAt(x) + '.'
+function getInitials(string) {
+	let x = string.indexOf(" ") + 1;
+	return string[0] + "." + string.charAt(x) + ".";
 }
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
  ********************************************************************/
 
-if (typeof yell === 'undefined') {
-  yell = undefined;
+if (typeof yell === "undefined") {
+	yell = undefined;
 }
 
-if (typeof getFirstCharacter === 'undefined') {
-  getFirstCharacter = undefined;
+if (typeof getFirstCharacter === "undefined") {
+	getFirstCharacter = undefined;
 }
 
-if (typeof getLastCharacter === 'undefined') {
-  getLastCharacter = undefined;
+if (typeof getLastCharacter === "undefined") {
+	getLastCharacter = undefined;
 }
 
-if (typeof getOneCharacter === 'undefined') {
-  getOneCharacter = undefined;
+if (typeof getOneCharacter === "undefined") {
+	getOneCharacter = undefined;
 }
 
-if (typeof getTwoCharacters === 'undefined') {
-  getTwoCharacters = undefined;
+if (typeof getTwoCharacters === "undefined") {
+	getTwoCharacters = undefined;
 }
 
-if (typeof makeCapitalized === 'undefined') {
-  makeCapitalized = undefined;
+if (typeof makeCapitalized === "undefined") {
+	makeCapitalized = undefined;
 }
 
-if (typeof yellLouder === 'undefined') {
-  yellLouder = undefined;
+if (typeof yellLouder === "undefined") {
+	yellLouder = undefined;
 }
 
-if (typeof getInitials === 'undefined') {
-  getInitials = undefined;
+if (typeof getInitials === "undefined") {
+	getInitials = undefined;
 }
-
 
 module.exports = {
-  yell,
-  getFirstCharacter,
-  getLastCharacter,
-  getOneCharacter,
-  getTwoCharacters,
-  makeCapitalized,
-  yellLouder,
-  getInitials,
+	yell,
+	getFirstCharacter,
+	getLastCharacter,
+	getOneCharacter,
+	getTwoCharacters,
+	makeCapitalized,
+	yellLouder,
+	getInitials,
 };
